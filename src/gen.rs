fcbb6d1a05739a6085d27800c06aad48e7a8f590
Nathan Stocks <cleancut@github.com>
2023-08-12 11:06:34 -0600

    add manual glue to expose generated code to the 'examine' library, add dependencies used by the generated code

---
+++ src/gen.rs
@@ -0,0 +1,7 @@
+#![allow(non_snake_case)]
+
+pub mod examine {
+    pub mod v1 {
+        include!("gen/examine.v1.rs");
+    }
+}
---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+// Module to contain generated items
+pub mod gen;
+
+// Expose the generated items at the root library level for convenience
+pub use gen::examine::v1::*;
